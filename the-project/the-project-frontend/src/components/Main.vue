01a7a14b62fb8273e7d7fbb1ef2baaf124379581
BurningBagel <luiz.duarte11@yahoo.com.br>
2026-08-17 18:31:45 -0300

    next exercise done, just need to make it pretty, also dev mode in frontend img src

--- the-project/the-project-frontend/src/components/Main.vue
+++ the-project/the-project-frontend/src/components/Main.vue
@@ -1,8 +1,41 @@
 <template>
-    <div style="flex">
+    <div style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
         <h1>The Project</h1>
-        <img src="/usr/src/app/data/lorem_img.png" alt="random image from lorem picsum">
+        <!-- <img src="/usr/src/app/data/lorem_img.png" alt="random image from lorem picsum"> -->
+        <img src="https://picsum.photos/1200" alt="random image">
+        <div class="entry">
+            <input type="text" max-length="140" v-model="inputText" placeholder="Enter a new todo here, up to 140 characters" />
+            <button @click="$emit('add-todo', inputText); inputText = ''">Send</button>
+        </div>
+
+        <h2>Todos</h2>
+
+        <div class="todos">
+            <TodoItem title="Todo 1" />
+            <TodoItem title="Todo 2" />
+            <TodoItem title="Todo 3" />
+        </div>
+
     </div>
 </template>
 
-<!-- TODO switch src out for image in directory -->
+<style lang="css" scoped>
+.entry {
+    display: flex;
+    flex-direction: row;
+}
+
+.todos {
+    display: flex;
+    flex-direction: column;
+}
+
+
+</style>
+
+<script setup lang="ts">
+import TodoItem from './TodoItem.vue';
+
+let inputText = '';
+
+</script>
---
+++ the-project/the-project-frontend/src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class="todo-item">
+        {{ title }}
+    </div>
+
+
+
+</template>
+
+<style lang="css" scoped>
+.todo-item {
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);   
+}
+
+
+</style>
+
+<script setup lang="ts">
+defineProps({
+    title: {
+        type: String
+    }
+})
+
+
+</script>
