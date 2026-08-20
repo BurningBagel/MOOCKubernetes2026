968cb4413009fb85603c2de75efd463721c2dc2f
BurningBagel <luiz.duarte11@yahoo.com.br>
2026-08-20 15:12:45 -0300

    styling done

--- the-project/the-project-frontend/src/components/Main.vue
+++ the-project/the-project-frontend/src/components/Main.vue
@@ -1,16 +1,16 @@
 <template>
-    <div style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
-        <h1>The Project</h1>
+    <div class="container" >
+        <h1 style="font-weight: bold; color: black;">The Project</h1>
         <!-- <img src="/usr/src/app/data/lorem_img.png" alt="random image from lorem picsum"> -->
-        <img src="https://picsum.photos/1200" alt="random image">
-        <div class="entry">
-            <input type="text" max-length="140" v-model="inputText" placeholder="Enter a new todo here, up to 140 characters" />
+        <img src="https://picsum.photos/1200" height="300px" width="300px" alt="random image">
+        <div class="entry_container">
+            <input type="text" max-length="140" v-model="inputText" placeholder="Enter a new todo here, max 140 characters" />
             <button @click="$emit('add-todo', inputText); inputText = ''">Send</button>
         </div>
 
-        <h2>Todos</h2>
+        <h2 style="font-weight: bold; color: black;">Todos</h2>
 
-        <div class="todos">
+        <div class="todos_container">
             <TodoItem title="Todo 1" />
             <TodoItem title="Todo 2" />
             <TodoItem title="Todo 3" />
@@ -20,14 +20,63 @@
 </template>
 
 <style lang="css" scoped>
-.entry {
-    display: flex;
-    flex-direction: row;
+
+TodoItem{
+    width: 100%;
+    border-radius: 5px;
 }
 
-.todos {
+img {
+    border-radius: 10%;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    -webkit-box-shadow: 0px 5px 11px 1px #000000; 
+    box-shadow: 0px 5px 11px 1px #000000;
+
+}
+
+input {
+    margin-right: 10px;
+    padding-right: 200px;
+    font-size: large;
+    border: 2px solid green;
+    border-radius: 5px;
+    background-color: #EEEEEE;
+}
+
+button {
+    font-size: large;
+    background-color: green;
+    border-width: 0;
+    border-radius: 5px;
+    padding: 10px;
+}
+
+input::placeholder {
+    overflow:visible;
+}
+
+.container {
+    display: flex; 
+    flex-direction: column; 
+    align-items: center; 
+    justify-content: space-around;
+    background-color: #EEEEEE;
+
+}
+
+.entry_container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-bottom: 50px;
+
+}
+
+.todos_container {
     display: flex;
     flex-direction: column;
+    width: 80%;
 }
 
 
--- the-project/the-project-frontend/src/components/TodoItem.vue
+++ the-project/the-project-frontend/src/components/TodoItem.vue
@@ -10,7 +10,15 @@
 
 <style lang="css" scoped>
 .todo-item {
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);   
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+    margin-top: 10px;
+    margin-bottom: 10px;
+    width: 100%;
+    text-align: left;
+    color: black;
+    padding: 10px;
+    border-radius: 5px;
+    border-left: 4px green solid;
 }
 
 
